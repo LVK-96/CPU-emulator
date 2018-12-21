ac4f4d2a821482a742527012c3cfde9b2f791676
leo <leo.kivikunnas@aalto.fi>
2018-12-21 17:05:14 +0200

    Started working on cpu class

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,1 +1,6 @@
 #include "cpu.hpp"
+
+CPU::CPU(): is_paused(true) {
+    clock_ = Clock();
+    ram_ = RAM();
+}
--- src/cpu.hpp
+++ src/cpu.hpp
@@ -1,7 +1,46 @@
 #pragma once
 
+#include "alu.hpp"
+#include "cu.hpp"
+#include "bus.hpp"
+#include "clock.hpp"
+#include "ram.hpp"
+#include "register.hpp"
+
 
 class CPU {
     public:
+        CPU();
+
+        RAM get_RAM();
+        void set_RAM();
+
+        Register get_programCounter(); 
+        Register get_memoryAddresReg();
+        Register get_instructionReg(); // holds instruction currenty being excecuted
+        Register get_A();
+        Register get_B();
+        Register get_outputReg();
+
+        void start();
+        void stop();
+        void onClock();
+
     private:
+        bool is_paused;
+        
+        Clock clock_;
+        RAM ram_;
+
+        Register programCounter_;
+        Register memomryAddressReg_;
+        Register instructionReg_;
+        Register A_;
+        Register B_;
+        Register outputReg_;
+        
+        Bus systemBus_;
+
+        ALU alu_;
+        CU cu_;
 };
